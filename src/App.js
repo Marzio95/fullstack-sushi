948c1f47ec3153d0f04a0ae2d78ad2a081da8bc6
Marzio95 <marziodellarocca14@gmail.com>
2022-09-12 16:48:19 +0200

    implemento l incremento della quantità ad un click

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import shrimp from './images/shrimp.png';
 class App extends Component {
 
   state = {cards: [
-    {id: 0, nome: "California", prezzo: 1.99, immagine: california},
+    {id: 0, nome: "California", prezzo: 1.99, immagine: california, quantità: 0},
     {id: 1, nome: "Dragon", prezzo: 3.49, immagine: dragon, quantità: 0},
     {id: 2, nome: "Dynamite", prezzo: 2.49, immagine: dynamite, quantità: 0},
     {id: 3, nome: "Philadelphia", prezzo: 0.99, immagine: philadelphia, quantità: 0},
@@ -25,6 +25,13 @@ class App extends Component {
     this.setState({cards: updateCards});
   }
 
+  handleIncrement = cardID => {
+    const updateCards = [...this.state.cards];
+    const myCard = updateCards.find(card => card.id === cardID);
+    myCard.quantità++;
+    this.setState({cards : updateCards});
+  }
+
   render() {
     return (
       <>
@@ -34,7 +41,7 @@ class App extends Component {
           <hr />
           <div className='row'>
             {this.state.cards.map(card => (
-              <Card key={card.id} id={card.id} nome={card.nome} prezzo={card.prezzo} immagine={card.immagine} onDelete={this.handleDelete} />
+              <Card key={card.id} id={card.id} nome={card.nome} prezzo={card.prezzo} immagine={card.immagine} onDelete={this.handleDelete} onIncrement={this.handleIncrement} quantità={card.quantità} />
             ))}
           </div>
         </div>
--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+
 import { Component } from 'react';
 
 class Card extends Component {
@@ -6,6 +6,7 @@ class Card extends Component {
         return(
             <div className="col">
                 <div className="card" style={{width: '18rem', textAlign: 'center'}}>
+                    <button onClick={() => this.props.onIncrement(this.props.id)} className="bg-primary">Quantità: {this.props.quantità}</button>
                     <img src={this.props.immagine} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{this.props.nome} Roll</h5>
